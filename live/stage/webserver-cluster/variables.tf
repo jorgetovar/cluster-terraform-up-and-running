cef0d20b40f203f8023f18611e3303808f10f5df
Jorge Tovar <jorgetovar621@gmail.com>
2024-05-31 16:37:38 -0500

    first commit

---
+++ live/stage/webserver-cluster/variables.tf
@@ -0,0 +1,11 @@
+variable "cluster_name" {
+  description = "The name to use to namespace all the resources in the cluster"
+  type        = string
+  default     = "webservers-stage"
+}
+
+variable "server_text" {
+  description = "The text for each EC2 instance to display. You can change this text to force a redeploy."
+  type        = string
+  default     = "Hello world"
+}
